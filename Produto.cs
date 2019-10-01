9bde94431751c7b27b02a4d5b5889c42134d0ac3
Alessandro Sampaio <alessandrosampaio521@gmail.com>
2019-09-30 22:47:16 -0300

    Adicionado model e classe de conexao

---
+++ Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorretorEAN
+{
+    public class Produto
+    {
+        public string Codigo { get; set; }
+        public string Ean { get; set; }
+        public string Descricao { get; set; }
+        public string Reduzida { get; set; }
+        public string Secao { get; set; }
+        public string Seccod { get; set; }
+        public string Grupo { get; set; }
+        public string Grpcod { get; set; }
+        public string Subgrupo { get; set; }
+        public string Sgrcod { get; set; }
+        public string NCM { get; set; }
+        public string CEST { get; set; }
+    }
+}
